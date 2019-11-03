2a99cead78326a2a827074713f7a4e51a57efee6
Katy Carr <khcarr9@gmail.com>
2019-11-03 14:27:16 -0500

    social links styling

--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import '../social-links.scss';
-import warren from '../assets/warren.png';
-import facebook from '../assets/facebook.png';
-import twitter from '../assets/twitter.png';
+import facebook from '../assets/social-Facebook.svg';
+import twitter from '../assets/social-Twitter.svg';
+import email from '../assets/social-Email.svg';
+import instagram from '../assets/social-Insta.svg';
 
 class SocialLinks extends Component {
   state = { dimAll: false };
@@ -28,15 +26,7 @@ class SocialLinks extends Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faInstagram} className="social__icon" />
-        </a>
-        <a
-          href="https://www.facebook.com/groups/2399105493707356/?ref=share"
-          className={wrapperClass}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={facebook} alt="facebook" className="social__icon" />
+          <img src={instagram} alt="instagram" className="social__icon" />
         </a>
         <a
           href="http://twitter.com/queensforwarren"
@@ -47,20 +37,20 @@ class SocialLinks extends Component {
           <img src={twitter} alt="twitter" className="social__icon" />
         </a>
         <a
+          href="https://www.facebook.com/groups/2399105493707356/?ref=share"
+          className={wrapperClass}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={facebook} alt="facebook" className="social__icon" />
+        </a>
+        <a
           href="mailto:queensforwarren@gmail.com"
           className={wrapperClass}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FontAwesomeIcon icon={faEnvelope} className="social__icon" />
-        </a>
-        <a
-          href="https://community.elizabethwarren.com/"
-          className={wrapperClass}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={warren} alt="Warren Community" className="social__icon" />
+          <img src={email} alt="email" className="social__icon" />
         </a>
       </div>
     );
